57fa65a9121b722f6d478dea5281af426413378c
Shivansh Kuchchal <shivanshbox@gmail.com>
2025-02-18 19:19:57 +0530

    unit tests for auth handler validations

--- internal/handler/handler_auth_test.go
+++ internal/handler/handler_auth_test.go
@@ -8,42 +8,56 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/require"
+
+	"github.com/shivanshkc/authorizer/internal/config"
+	"github.com/shivanshkc/authorizer/pkg/oauth"
 )
 
 func TestHandler_Auth_Validations(t *testing.T) {
+	mProvider := &mockProvider{name: "google"}
+	mHandler := &Handler{config: config.Config{AllowedRedirectURLs: []string{"https://allowed.com"}}}
+
 	for _, tc := range []struct {
 		name             string
+		mockProvider     oauth.Provider
 		inputProvider    string
 		inputRedirectURL string
 		errSubstring     string
 	}{
 		{
 			name:          "Too long provider length",
-			inputProvider: strings.Repeat("a", 100),
+			inputProvider: strings.Repeat("a", 21),
 			errSubstring:  errInvalidProvider.Error(),
 		},
 		{
 			name:          "Invalid provider character",
-			inputProvider: "google$$",
+			inputProvider: mProvider.name + "$$",
 			errSubstring:  errInvalidProvider.Error(),
 		},
 		{
 			name:             "Absent redirect_url",
-			inputProvider:    "google",
+			inputProvider:    mProvider.name,
 			inputRedirectURL: "",
 			errSubstring:     errInvalidCCU.Error(),
 		},
 		{
 			name:             "Too long redirect_url",
-			inputProvider:    "google",
-			inputRedirectURL: strings.Repeat("a", 300),
+			inputProvider:    mProvider.name,
+			inputRedirectURL: strings.Repeat("a", 201),
 			errSubstring:     errInvalidCCU.Error(),
 		},
 		{
 			name:             "redirect_url not present in allow list",
-			inputProvider:    "google",
-			inputRedirectURL: "https://my-random-url.com",
+			inputProvider:    mProvider.name,
+			inputRedirectURL: mHandler.config.AllowedRedirectURLs[0] + "-random",
 			errSubstring:     errUnknownRedirectURL.Error(),
+		},
+		{
+			name:             "unknown provider",
+			mockProvider:     mProvider,
+			inputProvider:    mProvider.name + "-random",
+			inputRedirectURL: mHandler.config.AllowedRedirectURLs[0],
+			errSubstring:     errUnsupportedProvider.Error(),
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
@@ -61,8 +75,9 @@ func TestHandler_Auth_Validations(t *testing.T) {
 			q.Set("redirect_url", tc.inputRedirectURL)
 			req.URL.RawQuery = q.Encode()
 
-			// Method to test.
-			(&Handler{}).Auth(rr, req)
+			// Prepare and call the method to test.
+			mHandler.googleProvider = tc.mockProvider
+			mHandler.Auth(rr, req)
 
 			// Verifications.
 			require.Equal(t, http.StatusBadRequest, rr.Code, "Expected 400 status code")
---
+++ internal/handler/mock_provider_test.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/shivanshkc/authorizer/pkg/oauth"
+)
+
+// mockProvider is a mock implementation of the oauth.Provider interface.
+type mockProvider struct {
+	// To mock the Name method.
+	name string
+	// To mock the GetAuthURL method.
+	authURL string
+	// To mock the TokenFromCode method.
+	errTokenFromCode error
+	token            string
+	// To mock the DecodeToken method.
+	errDecodeToken error
+	claims         oauth.Claims
+}
+
+func (m *mockProvider) Name() string {
+	return m.name
+}
+
+func (m *mockProvider) GetAuthURL(context.Context, string) string {
+	return m.authURL
+}
+
+func (m *mockProvider) TokenFromCode(context.Context, string) (string, error) {
+	if m.errTokenFromCode != nil {
+		return "", m.errTokenFromCode
+	}
+	return m.token, nil
+}
+
+func (m *mockProvider) DecodeToken(context.Context, string) (oauth.Claims, error) {
+	if m.errDecodeToken != nil {
+		return oauth.Claims{}, m.errDecodeToken
+	}
+	return m.claims, nil
+}
