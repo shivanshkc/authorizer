c354fcf5e17c9fbd8ad3cf064abaf27d9bed6783
Shivansh Kuchchal <shivanshbox@gmail.com>
2025-08-24 01:30:14 +0530

    refactor(core): remove signals package

--- cmd/authorizer/main.go
+++ cmd/authorizer/main.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/golang-migrate/migrate/v4"
 
@@ -16,7 +19,6 @@ import (
 	"github.com/shivanshkc/authorizer/internal/middleware"
 	"github.com/shivanshkc/authorizer/internal/repository"
 	"github.com/shivanshkc/authorizer/pkg/oauth"
-	"github.com/shivanshkc/authorizer/pkg/signals"
 
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
@@ -28,34 +30,25 @@ const googleScopes = "https://www.googleapis.com/auth/userinfo.email " +
 	"https://www.googleapis.com/auth/userinfo.profile"
 
 func main() {
-	// All signals.Xxx calls are for interruption (SIGINT, SIGTERM) handling.
-	// Wait blocks until all actions (registered by signals.OnSignal) have executed.
-	defer signals.Wait()
-	// Manually trigger cleanup whenever main exits.
-	// This MUST run before signals.Wait and so it is deferred after it.
-	defer signals.Manual()
-
 	// Root application context.
-	ctx, ctxCancel := context.WithCancel(context.Background())
-	// Cancel root context upon interruption or exit.
-	signals.OnSignal(func(signal os.Signal) { ctxCancel(); slog.Info("Root context canceled") })
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
 
 	// Initialize basic dependencies.
 	conf := config.Load()
 	logger.Init(os.Stdout, conf.Logger.Level, conf.Logger.Pretty)
 
+	// Setup the database.
 	database, err := connectDatabaseAndRunMigrations(ctx, conf)
 	if err != nil {
 		panic("failed to connect database and run migrations: " + err.Error())
 	}
 
-	// Close database upon interruption or exit.
-	signals.OnSignal(func(signal os.Signal) { _ = database.Close(); slog.Info("Database connection closed") })
-
 	// Instantiate the OAuth client for Google.
 	gCallback := fmt.Sprintf("%s/api/auth/google/callback", conf.Application.BaseURL)
 	gProvider, err := oauth.NewGoogle(ctx, conf.Google.ClientID, conf.Google.ClientSecret, gCallback, googleScopes)
 	if err != nil {
+		cleanup(database, nil)
 		panic("failed to initialize google provider: " + err.Error())
 	}
 
@@ -66,14 +59,14 @@ func main() {
 		Handler:    handler.NewHandler(conf, gProvider, nil, repository.NewRepository(database)),
 	}
 
-	// Shutdown server upon interruption or exit.
-	signals.OnSignal(func(_ os.Signal) { server.Shutdown() })
+	go func() {
+		if err := server.Start(); err != nil {
+			panic("error in server.Start call: " + err.Error())
+		}
+	}()
 
-	// This internally calls ListenAndServe.
-	// This is a blocking call and will panic if the server is unable to start.
-	if err := server.Start(); err != nil {
-		panic("error in server.Start call: " + err.Error())
-	}
+	<-ctx.Done()
+	cleanup(database, server)
 }
 
 func connectDatabaseAndRunMigrations(ctx context.Context, conf config.Config) (*sql.DB, error) {
@@ -106,5 +99,19 @@ func connectDatabaseAndRunMigrations(ctx context.Context, conf config.Config) (*
 		return nil, fmt.Errorf("failed to run migrations: %w", err)
 	}
 
+	slog.InfoContext(ctx, "Successfully ran database migrations")
 	return database, nil
+}
+
+func cleanup(database *sql.DB, server *http.Server) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if database != nil {
+		_ = database.Close()
+	}
+
+	if server != nil {
+		server.Shutdown(ctx)
+	}
 }
